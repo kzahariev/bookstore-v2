751b0e28dc988daa309fff753c35d58ea9038887
Krum Zahariev <kbzahariev@gmail.com>
2021-05-23 17:38:50 +0300

    commit

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -5,7 +5,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from "../components/CheckoutForm";
 
 const Checkout = () => {
-    const stripePromise = loadStripe('<stripe_public_key>');
+    const stripePromise = loadStripe('<pk_test_51IuI8zFSih8yKYQ339TuSTTU3eXPAOWVQ8a3JT3mYNEQk9zGgQXGRYZU5zjHOEIFvXEueS0dEwlHtnF7iW3lLpNp00swyIwAVl>');
 
     return (
         <section className="checkout-wrapper">
